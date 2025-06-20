4764e60ffe7fe0b85e28dfd1b842292f98bc4582
Camille Bouvat <mail@cbouvat.com>
2025-06-20 16:15:18 +0200

    Update blocklist version to 3.0 and add DVS Connect phone number

--- saracroche/SaracrocheViewModel.swift
+++ saracroche/SaracrocheViewModel.swift
@@ -23,7 +23,7 @@ class SaracrocheViewModel: ObservableObject {
   @Published var blockerPhoneNumberBlocked: Int64 = 0
   @Published var blockerPhoneNumberTotal: Int64 = 0
   @Published var blocklistInstalledVersion: String = ""
-  @Published var blocklistVersion: String = "2.0"
+  @Published var blocklistVersion: String = "3.0"
   @Published var showBlockerStatusSheet: Bool = false
 
   private var statusTimer: Timer? = nil
@@ -31,22 +31,119 @@ class SaracrocheViewModel: ObservableObject {
 
   // List of phone number patterns to block
   let blockPhoneNumberPatterns: [String] = [
-    "33162XXXXXX", // ARCEP
-    "33163XXXXXX", // ARCEP
-    "33270XXXXXX", // ARCEP
-    "33271XXXXXX", // ARCEP
-    "33377XXXXXX", // ARCEP
-    "33378XXXXXX", // ARCEP
-    "33424XXXXXX", // ARCEP
-    "33425XXXXXX", // ARCEP
-    "33568XXXXXX", // ARCEP
-    "33569XXXXXX", // ARCEP
-    "33948XXXXXX", // ARCEP
-    "339475XXXXX", // ARCEP
-    "339476XXXXX", // ARCEP
-    "339477XXXXX", // ARCEP
-    "339478XXXXX", // ARCEP
-    "339479XXXXX", // ARCEP
+    // ARCEP
+    "33162XXXXXX",
+    "33163XXXXXX",
+    "33270XXXXXX",
+    "33271XXXXXX",
+    "33377XXXXXX",
+    "33378XXXXXX",
+    "33424XXXXXX",
+    "33425XXXXXX",
+    "33568XXXXXX",
+    "33569XXXXXX",
+    "33948XXXXXX",
+    "339475XXXXX",
+    "339476XXXXX",
+    "339477XXXXX",
+    "339478XXXXX",
+    "339479XXXXX",
+
+    // DVS Connect : DVSC
+    "3316224XXXX",
+    "33162497XXX",
+    "33162498XXX",
+    "33162499XXX",
+    "33162525XXX",
+    "33186706XXX",
+    "3318844XXXX",
+    "33188441XXX",
+    "33188442XXX",
+    "33188443XXX",
+    "33188444XXX",
+    "33188445XXX",
+    "33188446XXX",
+    "33188447XXX",
+    "33188448XXX",
+    "33188449XXX",
+    "33189366XXX",
+    "3325959XXXX",
+    "33259591XXX",
+    "33259592XXX",
+    "33259593XXX",
+    "33259594XXX",
+    "33259595XXX",
+    "33259596XXX",
+    "33259597XXX",
+    "33259598XXX",
+    "33259599XXX",
+    "3327023XXXX",
+    "33270252XXX",
+    "33270253XXX",
+    "33270254XXX",
+    "3337647XXXX",
+    "33376471XXX",
+    "33376472XXX",
+    "33376473XXX",
+    "33376474XXX",
+    "33376475XXX",
+    "33376476XXX",
+    "33376477XXX",
+    "33376478XXX",
+    "33376479XXX",
+    "3337723XXXX",
+    "33377252XXX",
+    "33377253XXX",
+    "33377254XXX",
+    "3342423XXXX",
+    "33424431XXX",
+    "33424432XXX",
+    "33424433XXX",
+    "3345163XXXX",
+    "33451631XXX",
+    "33451632XXX",
+    "33451633XXX",
+    "33451634XXX",
+    "33451635XXX",
+    "33451636XXX",
+    "33451637XXX",
+    "33451638XXX",
+    "33451639XXX",
+    "3353716XXXX",
+    "33537161XXX",
+    "33537162XXX",
+    "33537163XXX",
+    "33537164XXX",
+    "33537165XXX",
+    "33537166XXX",
+    "33537167XXX",
+    "33537168XXX",
+    "33537169XXX",
+    "3356823XXXX",
+    "33568431XXX",
+    "33568432XXX",
+    "33568433XXX",
+    "33939401XXX",
+    "3394808XXXX",
+    "33948081XXX",
+    "33948082XXX",
+    "33948083XXX",
+    "3394820XXXX",
+    "33948455XXX",
+    "33974071XXX",
+    "3397472XXXX",
+    "33974721XXX",
+    "33974722XXX",
+    "33974723XXX",
+    "33974724XXX",
+    "33974725XXX",
+    "33974726XXX",
+    "33974727XXX",
+    "33974728XXX",
+    "33974729XXX",
+    "33987282XXX",
+    "33987283XXX",
+    "33987284XXX",
   ]
 
   let sharedUserDefaults = UserDefaults(
