3bfbbd31b0f572a9490b65b7fa854d10e1265bbd
Camille Bouvat <mail@cbouvat.com>
2026-01-03 20:15:01 +0100

    Remove documentation comments from HTTPMethod

--- saracroche/Utilities/Enums/HTTPMethod.swift
+++ saracroche/Utilities/Enums/HTTPMethod.swift
@@ -1,10 +1,7 @@
 import Foundation
 
-/// Represents HTTP methods used for network requests.
+/// HTTP methods for network requests
 enum HTTPMethod: String {
-  /// The HTTP GET method, used for retrieving data from the server.
   case get = "GET"
-
-  /// The HTTP POST method, used for sending data to the server.
   case post = "POST"
 }
