da560466b98e7c9449c43e8f15eb78cd31d11b77
Camille Bouvat <mail@cbouvat.com>
2026-01-04 10:41:24 +0100

    Comment out force update check in BlockerViewModel

--- saracroche/ViewModels/BlockerViewModel.swift
+++ saracroche/ViewModels/BlockerViewModel.swift
@@ -42,7 +42,7 @@ class BlockerViewModel: ObservableObject {
     }
 
     checkBlockerExtensionStatus()
-    checkAndForceUpdateIfNeeded()
+    //checkAndForceUpdateIfNeeded()
   }
 
   func checkBlockerExtensionStatus() {
