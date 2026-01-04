6b3e0e75c8c049c64352c53dfe7d1ea22919e9cb
Camille Bouvat <mail@cbouvat.com>
2026-01-04 18:22:25 +0100

    Add numbers management to SharedUserDefaultsService

--- saracroche/Services/SharedUserDefaultsService.swift
+++ saracroche/Services/SharedUserDefaultsService.swift
@@ -7,6 +7,7 @@ class SharedUserDefaultsService {
   // MARK: - Constants
   private struct Keys {
     static let action = "action"
+    static let numbers = "numbers"
   }
 
   init() {
@@ -18,16 +19,38 @@ class SharedUserDefaultsService {
     userDefaults?.set(action, forKey: Keys.action)
   }
 
+  /// Clear the action
   func clearAction() {
     userDefaults?.set("", forKey: Keys.action)
   }
 
+  /// Get the action
   func getAction() -> String {
     return userDefaults?.string(forKey: Keys.action) ?? ""
+  }
+
+  // MARK: - Numbers
+
+  /// Set the numbers array for processing by the CallDirectory extension
+  /// - Parameter numbers: Array of dictionaries containing number and optional name
+  func setNumbers(_ numbers: [[String: Any]]) {
+    userDefaults?.set(numbers, forKey: Keys.numbers)
+  }
+
+  /// Clear the numbers array
+  func clearNumbers() {
+    userDefaults?.set([], forKey: Keys.numbers)
+  }
+
+  /// Get the numbers array
+  /// - Returns: Array of dictionaries containing number and optional name
+  func getNumbers() -> [[String: Any]] {
+    return userDefaults?.array(forKey: Keys.numbers) as? [[String: Any]] ?? []
   }
 
   // MARK: - Reset All
   func resetAllData() {
     userDefaults?.removeObject(forKey: Keys.action)
+    userDefaults?.removeObject(forKey: Keys.numbers)
   }
 }
