af2da69335e75778da0619b817289cd004e86f83
Camille Bouvat <mail@cbouvat.com>
2026-01-15 15:24:49 +0100

    refactor: simplify CoreDataStack initialization and structure

--- saracroche/Database/CoreDataStack.swift
+++ saracroche/Database/CoreDataStack.swift
@@ -2,40 +2,25 @@ import CoreData
 
 // Define an observable class to encapsulate all Core Data-related functionality.
 class CoreDataStack: ObservableObject {
-    static let shared = CoreDataStack()
+  static let shared = CoreDataStack()
 
-    // Create a persistent container as a lazy variable to defer instantiation until its first use.
-    lazy var persistentContainer: NSPersistentContainer = {
+  private init() {}
 
-        // Pass the data model filename to the container’s initializer.
-        let container = NSPersistentContainer(name: "DataModel")
+  // Create a persistent container as a lazy variable to defer instantiation until its first use.
+  lazy var persistentContainer: NSPersistentContainer = {
 
-        // Load any persistent stores, which creates a store if none exists.
-        container.loadPersistentStores { _, error in
-            if let error {
-                // Handle the error appropriately. However, it's useful to use
-                // `fatalError(_:file:line:)` during development.
-                fatalError("Failed to load persistent stores: \(error.localizedDescription)")
-            }
-        }
-        return container
-    }()
+    // Pass the data model filename to the container’s initializer.
+    let container = NSPersistentContainer(name: "DataModel")
 
-    private init() { }
-}
-
-extension CoreDataStack {
-    // Add a convenience method to commit changes to the store.
-    func save() {
-        // Verify that the context has uncommitted changes.
-        guard persistentContainer.viewContext.hasChanges else { return }
-
-        do {
-            // Attempt to save changes.
-            try persistentContainer.viewContext.save()
-        } catch {
-            // Handle the error appropriately.
-            print("Failed to save the context:", error.localizedDescription)
-        }
+    // Load any persistent stores, which creates a store if none exists.
+    container.loadPersistentStores { _, error in
+      if let error {
+        // Handle the error appropriately. However, it's useful to use
+        // `fatalError(_:file:line:)` during development.
+        fatalError("Failed to load persistent stores: \(error.localizedDescription)")
+      }
     }
+    return container
+  }()
+
 }
