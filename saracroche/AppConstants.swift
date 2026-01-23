c4c3facf26ce4d4cc9ab308e93cc90e567b80a8e
Camille Bouvat <mail@cbouvat.com>
2026-01-23 14:58:42 +0100

    feat: add max numbers per batch constant

--- saracroche/AppConstants.swift
+++ saracroche/AppConstants.swift
@@ -4,24 +4,15 @@ import Foundation
 struct AppConstants {
 
   static let appGroupIdentifier = "group.com.cbouvat.saracroche"
-
   static let callDirectoryExtensionIdentifier = "com.cbouvat.saracroche.blocker"
-
   static let coreDataModelName = "Database"
-
   static let apiBaseURL = "https://saracroche.org/api"
-
   static let apiReportURL = "\(apiBaseURL)/report"
-
   static let apiListsURL = "\(apiBaseURL)/lists"
-
   static let apiFrenchListURL = "\(apiBaseURL)/lists/french-list-arcep-operators"
-
   static let backgroundServiceIdentifier = "com.cbouvat.saracroche.background-update"
-
   static let backgroundUpdateInterval: TimeInterval = 4 * 60 * 60
-
   static let listDownloadInterval: TimeInterval = 24 * 60 * 60
-
   static let numberChunkSize = 10_000
+  static let maxNumbersPerBatch: Int64 = 1_000_000
 }
