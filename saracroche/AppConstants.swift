d714a1ada1f8692a0959bdf05444fd441fefde84
Camille Bouvat <mail@cbouvat.com>
2026-02-10 17:17:42 +0100

    chore: update pattern reprocess interval to 20 days

--- saracroche/AppConstants.swift
+++ saracroche/AppConstants.swift
@@ -15,6 +15,6 @@ struct AppConstants {
   static let listDownloadInterval: TimeInterval = 24 * 60 * 60
   static let numberChunkSize = 10_000
   static let maxNumbersPerBatch: Int64 = 1_000_000
-  static let patternReprocessInterval: TimeInterval = 15 * 24 * 60 * 60
+  static let patternReprocessInterval: TimeInterval = 20 * 24 * 60 * 60
   static let notificationReminderInterval: TimeInterval = 15 * 24 * 60 * 60
 }
